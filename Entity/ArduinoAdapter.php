977760d73caa90a5eb1369fb689e7d6682855568
David Domenech <david.domenechp@estudiants.urv.cat>
2024-12-04 17:26:29 +0100

    FIX

--- Entity/ArduinoAdapter.php
+++ Entity/ArduinoAdapter.php
@@ -2,12 +2,13 @@
 namespace Entity;
 
 class ArduinoAdapter implements SensorAdapter{
-
+    private const PRIVATE_KEY = 52039977;
     public function adapt(string $request):Ambient{
         [
             "temp" => $temp,
             "humitat" => $humitat,
-            "llum" => $llum
+            "llum" => $llum,
+            "signature" => $signature
         ] = json_decode($request, true);
 
         if ($temp == null || $humitat == null || $llum == null){
@@ -19,7 +20,19 @@ class ArduinoAdapter implements SensorAdapter{
         $ambient->setHumitat($humitat);
         $ambient->setLlum($llum);
 
+        if (!$this->verifySignature($ambient, $signature)){
+            throw new \Exception("Error en la signatura");
+        }
+
         return $ambient;
+    }
+
+    private function verifySignature(Ambient $ambient, string $signature){
+        //temp, humitat i llum concatenades + AES = signatura
+        $operacio = round($signature / (self::PRIVATE_KEY/100),2);
+        $sum= $ambient->getTemp() + $ambient->getHumitat() + $ambient->getLlum();
+
+        return $operacio == $sum;
     }
 
 }
